4119a3e7faab91c9a4bda44f6e563204f1903e96
ajiang13260 <ajiang13260@gmail.com>
2019-03-23 22:51:14 +0800

    wrote my code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var price = 6;
+    var distance = inputs.distance;
+    var parkTime = inputs.parkTime;
+
+
+    if (distance > 2 && distance <= 8) {
+        price = 6 + 0.8 * (distance-2);
+    }
+    else if(distance > 8){
+        price =  6 + 0.8 * 6 + (distance - 8) * 0.8 * 1.5;
+    }
+    if (parkTime != 0) {
+        price = price + 0.25 * parkTime;
+    }
+    price = Math.round(price)
+    return price;
 };
